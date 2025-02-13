c70c12f6d49b6a2fce84e0c949da6079d13f313e
salsabillaputri0 <2223612.salsabilla@smkn-2sbg.sch.id>
2025-02-13 13:41:47 +0700

    menambahkan komentar

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -7,15 +7,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
+    // Menggunakan trait HasFactory untuk memanfaatkan fitur pabrik (factory) pada model ini
     use HasFactory;
     
     /**
      * post
      *
-     * @return void
+     * Mendefinisikan relasi antara model Comment dengan model Post
+     * 
+     * Setiap komentar (Comment) berhubungan dengan satu post (Post).
+     * Fungsi ini menunjukkan bahwa Comment "belongs to" (milik) sebuah Post.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
      */
     public function post()
     {
+        // Menyatakan bahwa setiap komentar milik satu post tertentu
+        // Menggunakan relasi belongsTo untuk menghubungkan Comment ke Post
         return $this->belongsTo(Post::class);
     }
 }
--- app/Models/Phone.php
+++ app/Models/Phone.php
@@ -7,15 +7,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Phone extends Model
 {
+    // Menggunakan trait HasFactory untuk memanfaatkan fitur pabrik (factory) pada model ini
     use HasFactory;
     
     /**
      * user
      *
-     * @return void
+     * Mendefinisikan relasi antara model Phone dengan model User
+     * 
+     * Setiap nomor telepon (Phone) berhubungan dengan satu pengguna (User).
+     * Fungsi ini menunjukkan bahwa Phone "belongs to" (milik) sebuah User.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
      */
     public function user()
     {
+        // Menyatakan bahwa setiap nomor telepon milik satu pengguna tertentu
+        // Menggunakan relasi belongsTo untuk menghubungkan Phone ke User
         return $this->belongsTo(User::class);
     }
 }
