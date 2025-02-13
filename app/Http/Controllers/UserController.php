3e4786894c9123bd76da67f4aeb09f0e64ba799d
salsabillaputri0 <2223612.salsabilla@smkn-2sbg.sch.id>
2025-02-13 13:34:00 +0700

    menambahkan komentar

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,14 +9,19 @@ class UserController extends Controller
     /**
      * index
      *
+     * Menampilkan daftar semua pengguna yang ada di database
+     *
      * @return void
      */
     public function index()
     {
-        //get users form Model
+        // Mendapatkan semua data pengguna (users) dari model User
+        // Mengambil data pengguna terbaru berdasarkan waktu dibuat (menggunakan method latest())
+        // Method get() digunakan untuk mengambil seluruh data pengguna
         $users = User::latest()->get();
 
-        //passing user to view
+        // Mengirimkan data pengguna ke view 'users'
+        // Data pengguna akan tersedia dalam variabel $users di dalam view
         return view('users', compact('users'));
     }
 }
